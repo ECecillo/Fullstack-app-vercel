9e0c2607150e6cec5ceee58695e96cf4219d15d6
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-02-19 11:20:26 +0100

    Ajout dans le header de nouveau composant pour OAUth ajout d'un objet useSession qui va récupérer les infos de la session afficher des Link en conséquence pour lorsqu'il est connecté ou non.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+// Fonctionnalités pour l'authentification des users.
+import { signOut, useSession } from "next-auth/react";
 
 const Header: React.FC = () => {
   const router = useRouter();
   const isActive: (pathname: string) => boolean = (pathname) =>
     router.pathname === pathname;
+
+  // Permet de check si l'user est authentifié
+  const { data: session, status } = useSession();
+
+  // On destruct l'objet de retour de useSession() qui nous donne un objet data et status.
+
+  // data : Peut avoir 3 valeurs (Session/undefined/null) :
+  /* 
+    - quand la session n'a pas encore été demandé data = undefined.
+    - Si la Session n'a pas pu être étable alors data = null.
+
+    - Si ça a réussi data = {
+      user?: {
+        name?: string | null
+        email?: string | null
+        image?: string | null
+      }
+      expires?: string
+    }
+  */
 
   let left = (
     <div className="left">
@@ -38,6 +60,160 @@ const Header: React.FC = () => {
 
   let right = null;
 
+  // L'utilisateur est en cours de connexion ou pas auth.
+  if (status === 'loading') {
+    left = (
+      <div className="left">
+        <Link href="/">
+          <a className="bold" data-active={isActive('/')}>
+            Feed
+          </a>
+        </Link>
+        <style jsx>{`
+          .bold {
+            font-weight: bold;
+          }
+
+          a {
+            text-decoration: none;
+            color: var(--geist-foreground);
+            display: inline-block;
+          }
+
+          .left a[data-active='true'] {
+            color: gray;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+        `}</style>
+      </div>
+    );
+    right = (
+      <div className="right">
+        <p>Validating session ...</p>
+        <style jsx>{`
+          .right {
+            margin-left: auto;
+          }
+        `}</style>
+      </div>
+    );
+  }
+  // si session est null ou undefined alors on affiche un bouton de connexion.
+  if (!session) {
+    right = (
+      <div className="right">
+        <Link href="/api/auth/signin">
+          <a data-active={isActive('/signup')}>Log in</a>
+        </Link>
+        <style jsx>{`
+          a {
+            text-decoration: none;
+            color: var(--geist-foreground);
+            display: inline-block;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+
+          .right {
+            margin-left: auto;
+          }
+
+          .right a {
+            border: 1px solid var(--geist-foreground);
+            padding: 0.5rem 1rem;
+            border-radius: 3px;
+          }
+        `}</style>
+      </div>
+    );
+  }
+  // Si on est connecté on affiche la possibilité de regarder ces posts, créer un nouveau post, suivre ceux postés ou le bouton se déconnecté.
+  if (session) {
+    left = (
+      <div className="left">
+        <Link href="/">
+          <a className="bold" data-active={isActive('/')}>
+            Feed
+          </a>
+        </Link>
+        <Link href="/drafts">
+          <a data-active={isActive('/drafts')}>My drafts</a>
+        </Link>
+        <style jsx>{`
+          .bold {
+            font-weight: bold;
+          }
+
+          a {
+            text-decoration: none;
+            color: var(--geist-foreground);
+            display: inline-block;
+          }
+
+          .left a[data-active='true'] {
+            color: gray;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+        `}</style>
+      </div>
+    );
+    right = (
+      <div className="right">
+        <p>
+          {session.user.name} ({session.user.email})
+        </p>
+        <Link href="/create">
+          <button>
+            <a>New post</a>
+          </button>
+        </Link>
+        <button onClick={() => signOut()}>
+          <a>Log out</a>
+        </button>
+        <style jsx>{`
+          a {
+            text-decoration: none;
+            color: var(--geist-foreground);
+            display: inline-block;
+          }
+
+          p {
+            display: inline-block;
+            font-size: 13px;
+            padding-right: 1rem;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+
+          .right {
+            margin-left: auto;
+          }
+
+          .right a {
+            border: 1px solid var(--geist-foreground);
+            padding: 0.5rem 1rem;
+            border-radius: 3px;
+          }
+
+          button {
+            border: none;
+          }
+        `}</style>
+      </div>
+    );
+  }
+
+  // Affiche les composants que l'on a défini en fonction de session.
   return (
     <nav>
       {left}
